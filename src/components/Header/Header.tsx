88881ea88d3197179397bf33adf3edc6c298006a
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-03-10 17:48:37 -0500

    Updating the general information

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 		<header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
 			<div className="flex items-center w-full md:2/3">
 				<Link href="/" className="font-black text-tertiary-dark text-3xl">
-					Bonvoy
+					Roomify
 				</Link>
 				<ul className="flex items-center ml-5">
 					<li className="flex items-center">
